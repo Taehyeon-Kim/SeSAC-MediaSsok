0a457d16d12a971118b93805721190862f674f33
김태현 <taehyk528@gmail.com>
2022-08-04 02:50:05 +0900

    [#5] Image Base URL 추가

--- MovieSsok/MovieSsok/Common/Constants.swift
+++ MovieSsok/MovieSsok/Common/Constants.swift
@@ -9,6 +9,7 @@ import Foundation
 
 enum APIConstants {
     static let baseURL = "https://api.themoviedb.org/3"
+    static let imageBaseURL = "https://image.tmdb.org/t/p/w500"
     
     static let getTrendingURL = baseURL + "/trending"
 }
