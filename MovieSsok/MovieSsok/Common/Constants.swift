896bbd6a23fee48d50ac2655dc0095b495bf2888
김태현 <taehyk528@gmail.com>
2022-08-04 02:26:41 +0900

    [#5] APIConstants 생성

---
+++ MovieSsok/MovieSsok/Common/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MovieSsok
+//
+//  Created by taekki on 2022/08/04.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let baseURL = "https://api.themoviedb.org/3"
+    
+    static let getTrendingURL = baseURL + "/trending"
+}
