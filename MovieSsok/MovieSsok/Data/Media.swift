d8dcd0f0020948f7fd114e06d2a4844987a8819e
김태현 <taehyk528@gmail.com>
2022-08-04 02:32:38 +0900

    [#5] Media 모델 생성

---
+++ MovieSsok/MovieSsok/Data/Media.swift
@@ -0,0 +1,17 @@
+//
+//  Media.swift
+//  MovieSsok
+//
+//  Created by taekki on 2022/08/04.
+//
+
+import Foundation
+
+struct Media {
+    let releaseData: String
+    let title: String
+    let posterPath: String
+    let rate: Double
+    let originalTitle: String
+    let overview: String
+}
