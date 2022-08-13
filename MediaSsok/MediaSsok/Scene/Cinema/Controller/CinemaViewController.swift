f330f40b1f1f32a734ca91ffae6d4b06451a2f0b
김태현 <taehyk528@gmail.com>
2022-08-13 19:14:32 +0900

    [#12] CinemaViewController 파일 생성

---
+++ MediaSsok/MediaSsok/Scene/Cinema/Controller/CinemaViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CinemaViewController.swift
+//  MediaSsok
+//
+//  Created by taekki on 2022/08/13.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+final class CinemaViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
