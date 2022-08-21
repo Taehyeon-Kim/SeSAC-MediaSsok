9c94010fe5c99c369a678806e4d7f95025a2f25b
김태현 <taehyk528@gmail.com>
2022-08-21 18:04:03 +0900

    [#14] MediaSsok-UIKit Framework 생성

---
+++ MediaSsok/MediaSsok-UIKit/MediaSsok_UIKit.h
@@ -0,0 +1,18 @@
+//
+//  MediaSsok_UIKit.h
+//  MediaSsok-UIKit
+//
+//  Created by taekki on 2022/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MediaSsok_UIKit.
+FOUNDATION_EXPORT double MediaSsok_UIKitVersionNumber;
+
+//! Project version string for MediaSsok_UIKit.
+FOUNDATION_EXPORT const unsigned char MediaSsok_UIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediaSsok_UIKit/PublicHeader.h>
+
+
